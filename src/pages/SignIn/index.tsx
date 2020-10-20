876250aaee216da1cbf2160baca53bfdcac42e80
unknown <bittencourtcrb@gmail.com>
2020-10-20 14:07:18 -0300

    Input e Button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container, Title } from './styles';
-
 import logoImg from '../../assets/logo.png';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu logon</Title>
+
+      <Input name="mail" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="senha" />
+
+      <Button onPress={() => { }}>Entrar</Button>
     </Container>
   );
 };
